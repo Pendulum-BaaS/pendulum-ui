0b394f9dfd88185f8d327be0f15d8ac8eb3e2922
RachelRusiecki <rachrusiecki@gmail.com>
2025-07-24 09:48:34 -0500

    feat: add collection management table layout with checkboxes

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,14 +1,38 @@
 import { useState } from 'react';
 import { sampleCollections } from '../sample_data/sample_data';
 import NavigationBar from './NavigationBar';
+import DocumentTable from './DocumentTable';
 
 const Data = () => {
   const [activeCollection, setActiveCollection] = useState('');
-  const [selectedDocuments, setSelectedDocuments] = useState([]);
+  const [selectedDocuments, setSelectedDocuments] = useState<string[]>([]);
+
+  const selectedQty = selectedDocuments.length;
+  const currentCollection = sampleCollections.find(collection => {
+    return collection.name === activeCollection;
+  });
 
   const handleCollectionChange = (collectionName: string) => {
     setActiveCollection(collectionName);
     setSelectedDocuments([]);
+  };
+
+  const handleDocumentSelect = (documentId: string, selected: boolean) => {
+    if (selected) {
+      setSelectedDocuments(selectedDocuments.concat(documentId));
+    } else {
+      setSelectedDocuments(selectedDocuments.filter(doc => {
+        return doc !== documentId;
+      }));
+    };
+  };
+
+  const handleSelectAll = (selected: boolean) => {
+    if (selected && currentCollection) {
+      setSelectedDocuments(currentCollection.documents.map(({id}) => id));
+    } else {
+      setSelectedDocuments([]);
+    };
   };
 
   return (
@@ -17,6 +41,24 @@ const Data = () => {
         collections={sampleCollections}
         onCollectionChange={handleCollectionChange}
       ></NavigationBar>
+      <h2>{activeCollection}</h2>
+      <div>
+        {
+          selectedQty > 0 ?
+          `${selectedQty} document${selectedQty === 1 ? '' : 's'} selected` :
+          ''
+        }
+      </div>
+      {
+        activeCollection ?
+        <DocumentTable
+          documents={currentCollection ? currentCollection.documents : []}
+          selectedDocuments={selectedDocuments}
+          onDocumentSelect={handleDocumentSelect}
+          onSelectAll={handleSelectAll}
+        ></DocumentTable> :
+        ''
+      }
     </>
   );
 };
--- src/components/DocumentTable.tsx
+++ src/components/DocumentTable.tsx
@@ -1,5 +1,92 @@
-const DocumentTable = () => {
-  return 'Doc Table';
+import { useMemo } from 'react';
+import DocumentRow from './DocumentRow';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Checkbox
+} from '@mui/material';
+
+import type { DocumentTableProps } from '../types/types';
+
+const DocumentTable = ({
+  documents,
+  selectedDocuments,
+  onDocumentSelect,
+  onSelectAll
+}: DocumentTableProps) => {
+  const headers = useMemo(() => {
+    const allKeys = new Set<string>();
+    for (let doc of documents) {
+      for (let key of Object.keys(doc)) allKeys.add(key);
+    };
+    return Array.from(allKeys);
+  }, [documents]);
+
+  const allSelected = documents.length > 0 && documents.every(({id}) => {
+    return selectedDocuments.includes(id);
+  });
+
+  if (documents.length > 0) {
+    return (
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Checkbox
+                  checked={allSelected}
+                  onChange={() => onSelectAll(!allSelected)}
+                ></Checkbox>
+              </TableCell>
+              {headers.map(header => {
+                return <TableCell key={header}>{header}</TableCell>
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {documents.map(doc => {
+              return (
+                <DocumentRow
+                  key={doc.id}
+                  doc={doc}
+                  headers={headers}
+                  isSelected={selectedDocuments.includes(doc.id)}
+                  onDocumentSelect={onDocumentSelect}
+                ></DocumentRow>
+              )
+            })}
+            {/* {documents.map(doc => {
+              return (
+                <TableRow key={doc.id}>
+                  <TableCell>
+                    <Checkbox
+                      checked={selectedDocuments.includes(doc.id)}
+                      onChange={() => {
+                        onDocumentSelect(
+                          doc.id,
+                          !selectedDocuments.includes(doc.id)
+                        )
+                      }}
+                    ></Checkbox>
+                  </TableCell>
+                  {headers.map(header => {
+                    return <TableCell key={header}>{doc[header]}</TableCell>
+                  })}
+                </TableRow>
+              )
+            })} */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  } else {
+    return <p>No documents found in this collection</p>
+  }
+
 };
 
 export default DocumentTable;
