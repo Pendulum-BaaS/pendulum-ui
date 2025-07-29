c9f1650811e1d6030df2fd4c39b0124fb04912e7
Sean Lacey <seanlacey2164@gmail.com>
2025-07-29 00:16:25 -0400

    feat: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+export default function Header() {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: "inherit",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        width: "100vw",
+      }}
+    >
+      <Toolbar>
+        <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+          <img
+            src="src/assets/logo.png"
+            alt="Logo"
+            style={{ height: "40px" }}
+          ></img>
+        </Box>
+        <Button
+          variant="contained"
+          sx={{ background: "inherit", color: "inherit" }}
+        >
+          <LogoutIcon />
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
