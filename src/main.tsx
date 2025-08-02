fbf970c43d2047afbfba2b02d1f21fecd351e3dd
Sean Lacey <seanlacey2164@gmail.com>
2025-08-02 15:01:19 -0400

    chore: update basename of prod app to be server by app server

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <PendulumProvider>
     <LogsProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/admin">
         <App />
       </BrowserRouter>
     </LogsProvider>
